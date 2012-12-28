370dd53515d3510be5c65fe11292b712750a101d
Jared Hanson <jaredhanson@gmail.com>
2012-12-27 21:41:45 -0800

    Use Travis CI.

--- test/sasl-digest-md5.mechanism.test.js
+++ test/sasl-digest-md5.mechanism.test.js
@@ -1,7 +1,7 @@
 (function(root, factory) {
   if (typeof exports === 'object') {
     // CommonJS
-    factory(require('sasl-digest-md5/lib/mechanism'));
+    factory(require('../lib/mechanism'));
   } else if (typeof define === 'function' && define.amd) {
     // AMD
     define(['sasl-digest-md5/lib/mechanism'], factory);
